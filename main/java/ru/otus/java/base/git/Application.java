970f7d0036f2377ae18ed69ba78934e58c23cc43
YaGrib19 <YaGrib19@users.noreply.github.com>
2024-10-07 21:59:01 +0300

    Initial commit

---
+++ main/java/ru/otus/java/base/git/Application.java
@@ -0,0 +1,11 @@
+package ru.otus.java.base.git;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printOneTwo() {
+        System.out.print('yth')
+    }
+}
